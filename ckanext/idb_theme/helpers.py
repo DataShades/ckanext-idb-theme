c124decd5b733b6ee97082b3a75c3b5b2d51a778
Yan Rudenko <yan.rudenko@gmail.com>
2026-06-22 18:58:01 +0200

    IADBXCIGB-50  / Update banner logic and styling

--- ckanext/idb_theme/helpers.py
+++ ckanext/idb_theme/helpers.py
@@ -1,8 +1,50 @@
 from __future__ import annotations
 
-from . import config
+from typing import Any
 
+from . import config
+from ckan.common import _
+import ckan.plugins.toolkit as tk
+import ckan.model as model
 
 def idb_theme_default_collapsed(facet_name: str):
     """Check whether the facet with the given name should be collapsed by default."""
     return facet_name in config.collapsed_facets()
+
+
+def idb_theme_info_graph() -> dict[str, Any]:
+    """Get the info graph metrics for the homepage."""
+    return {
+        'metrics': [
+            {
+                'value': tk.h.get_dataset_count(),
+                'label': _('Available datasets'),
+            },
+            {
+                'value': idb_theme_get_group_count(type='organization'),
+                'label': _('Organizations'),
+            },
+            {
+                'value': '18+',
+                'label': _('Countries covered'),
+            },
+            {
+                'value': idb_theme_get_group_count(),
+                'label': _('Thematic groups'),
+            },
+        ],
+    }
+
+
+def idb_theme_get_group_count(type: str = 'group') -> dict[str, int]:
+    q = model.Session.query(model.Group)\
+        .filter(model.Group.type == type)\
+        .filter(model.Group.state == 'active')
+    if type == 'organization':
+        q = q.filter(model.Group.is_organization == True)
+    elif type == 'group':
+        q = q.filter(model.Group.is_organization == False)
+    else:
+        raise ValueError(f'Invalid group type: {type}')
+    return q.count()
+
